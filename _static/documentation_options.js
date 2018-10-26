6f5e824d0489be41f32bf47fbbd147dc9d57481b
Matthias Mohr <m.mohr@uni-muenster.de>
2018-10-26 14:21:30 +0000

    Deploy Open-EO/openeo-udf to github.com/Open-EO/openeo-udf.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.0.post0.dev86+gfe7f849',
+    VERSION: '0.0.post0.dev99+gfb10521',
     LANGUAGE: 'Python',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
