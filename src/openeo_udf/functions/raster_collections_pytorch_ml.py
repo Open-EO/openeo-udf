aa4650d74e686930b80ddbabe5f4a3fb18f861c8
huhabla <mo6i98ad>
2018-10-28 23:04:00 +0100

    Code cleanup

--- src/openeo_udf/functions/raster_collections_pytorch_ml.py
+++ src/openeo_udf/functions/raster_collections_pytorch_ml.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 # Uncomment the import only for coding support
 #import numpy
-#import torch
-#import torch.nn as nn
-#import torch.nn.functional as F
-#from torch.autograd import Variable
-#import torch
+import torch
 #import torchvision
 #import tensorflow
 #import tensorboard
-#from openeo_udf.api.base import SpatialExtent, RasterCollectionTile, FeatureCollectionTile, UdfData, MachineLearnModel
+from openeo_udf.api.base import SpatialExtent, RasterCollectionTile, FeatureCollectionTile, UdfData, MachineLearnModel
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -34,26 +30,17 @@ def rct_pytorch_ml(udf_data):
     """
     tile = udf_data.raster_collection_tiles[0]
 
-    # We need to reshape the data for prediction into one dimensional arrays
-    three_dim_shape = tile.data.shape
-    one_dim_shape = numpy.prod(three_dim_shape)
-
-    tile_reshape = tile.data.reshape(one_dim_shape)
-
     # This is the input data of the model.
-    input = Variable(torch.Tensor(tile_reshape))
+    input = torch.autograd.Variable(torch.Tensor(tile.data))
 
     # Get the first model
     mlm = udf_data.get_ml_model_list()[0]
     m = mlm.get_model()
     # Predict the data
     pred = m(input)
-    # Reshape the one dimensional predicted values to three dimensions based on the input shape
-    pred_reshape = pred.reshape(three_dim_shape)
-
     # Create the new raster collection tile
-    rct = RasterCollectionTile(id=mlm.name, extent=red.extent, data=pred_reshape,
-                               start_times=red.start_times, end_times=red.end_times)
+    rct = RasterCollectionTile(id=mlm.name, extent=tile.extent, data=numpy.array(pred.tolist()),
+                               start_times=tile.start_times, end_times=tile.end_times)
     # Insert the new tiles as list of raster collection tiles in the input object. The new tiles will
     # replace the original input tiles.
     udf_data.set_raster_collection_tiles([rct,])
