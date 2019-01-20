62f94988d27fe858bf16f7fbc001193f926408ab
huhabla <mo6i98ad>
2019-01-20 19:46:55 +0100

    Added hyper cube definition

--- src/openeo_udf/server/definitions.py
+++ src/openeo_udf/server/definitions.py
@@ -129,6 +129,115 @@ class RasterCollectionTile(Schema):
 
 #####################################################################
 
+class Dimension(Schema):
+    description = "Description of a single dimension. "
+    type = "object"
+    required = ["name", "unit"]
+    properties = {
+        "name": {
+            "description": "The name of the dimension, like *time*, *X*, *Y*, *Z* and so on.",
+            "type": "string"
+        },
+        "description": {
+            "description": "Description of the dimension.",
+            "type": "string"
+        },
+        "unit": {
+            "description": "The unit of the dimension.",
+            "type": "string"
+        }
+    }
+
+
+#####################################################################
+
+class HyperCube(Schema):
+    description = "A multi dimensional hyper cube that contains the same information as a raster collection tile but" \
+                  "with configurable dimensions. "
+    type = "object"
+    required = ["id", "data", "dimension", "extent"]
+    properties = {
+        "id": {
+            "description": "The identifier of this hyper cube.",
+            "type": "string"
+        },
+        "wavelength": {
+            "description": "The wavelength of the array value.",
+            "type": "float"
+        },
+        "data": {
+            "description": "A multi-dimensional array of integer (8,16,32,64 bit) or float (16, 32, 64 bit) values." \
+                           "By default index dimension is as follows: [time][y][x]. Hence, the index data[0] returns " \
+                           "the 2D slice for the first time-stamp. The y-indexing if counted from top to bottom " \
+                           "and represents the rows of the 2D array. The x-indexing is counted from left to right " \
+                           "and represents the columns of the 2D array. The dimension options must be used ot describe "
+                           "other dimensions.",
+            "type": "array",
+            "items": {
+                "type": "array",
+                "items": {
+                    "type": "array",
+                    "items": {"type": "float"}
+                }
+            }
+        },
+        "start_times": {
+            "description": "The array that contains that start time values for the time dimension."
+                           "As date-time string format ISO 8601 must be supported.",
+            "type": "array",
+            "items": {"type": "string"}
+        },
+        "end_times": {
+            "description": "The vector that contains that end time values for the time dimension, in case the "
+                           "the time stamps for all or a subset of slices are intervals. For time instances "
+                           "the from and to time stamps must be equal or empty. "
+                           "As date-time string format ISO 8601 must "
+                           "be supported",
+            "type": "array",
+            "items": {"type": "string"}
+        },
+        "dimensions": {
+            "description": "The description of each dimension as ordered list",
+            "type": "array",
+            "items": {
+                "type": Dimension
+            }
+        }
+    }
+    example = {
+        "id": "test_data",
+        "wavelength": 420,
+        "start_times": ["2001-01-01T00:00:00",
+                        "2001-01-02T00:00:00"],
+        "end_times": ["2001-01-02T00:00:00",
+                      "2001-01-03T00:00:00"],
+        "data": [
+            [
+                [0, 1],
+                [2, 3]
+            ],
+            [
+                [0, 1],
+                [2, 3]
+            ]
+        ],
+        "extent": {
+            "top": 53,
+            "bottom": 51,
+            "right": 30,
+            "left": 28,
+            "height": 1,
+            "width": 1
+        },
+        "dimension": [{"name": "time", "unit": "ISO:8601"},
+                      {"name": "X", "unit": "meter"},
+                      {"name": "Y", "unit": "meter"}
+                      ]
+    }
+
+
+#####################################################################
+
 class FeatureCollectionTile(Schema):
     description = "A tile of vector data that represents a " \
                   "spatio-temporal subset of a spatio-temporal " \
