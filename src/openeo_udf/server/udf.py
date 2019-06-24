10e9336a8a324f1f5dda9ed19477f4c91c5b19a2
huhabla <mo6i98ad>
2019-06-25 00:18:23 +0200

    Added binary serialization discussion

--- src/openeo_udf/server/udf.py
+++ src/openeo_udf/server/udf.py
@@ -44,6 +44,38 @@ POST_JOBS_DOC = {
 class Udf(Resource):
     @swagger.doc(POST_JOBS_DOC)
     def post(self):
+        """Execute the UDF code
+
+        TODO: Improve this method with with support for binary serialization
+
+        There are several different approached available in python that can be implemented:
+        - UBJSON https://en.wikipedia.org/wiki/UBJSON
+        - BSON https://en.wikipedia.org/wiki/BSON
+        - MessagePack https://en.wikipedia.org/wiki/MessagePack
+
+        Important is the support for arrays with integer and floating point numbers that are used in
+        xarray and numpy. Support for timestamps is required as well. For structured data is the support of lists
+        and maps important.
+
+                      array integer float map time
+
+        MessagePack     y      y      y    y    y
+        BJSON           y      y      y    y    n
+        UBJSON          n      y      y    n    n
+
+        Based on these requirement it seems that MessagePack is the most potent candidate to use
+        for serialization and supports many different languages.
+        MessagePack is available here: https://msgpack.org/
+
+        Using Messagepack is quite easy:
+
+        >>> d = dict(array=[1,2,3,4,5], name="Test", meta=dict(a="a", b=2))
+        >>> msgpack.packb(d)
+        b'\x83\xa5array\x95\x01\x02\x03\x04\x05\xa4name\xa4Test\xa4meta\x82\xa1a\xa1a\xa1b\x02'
+        >>> msgpack.unpackb(_)
+        {b'array': [1, 2, 3, 4, 5], b'name': b'Test', b'meta': {b'a': b'a', b'b': 2}}
+
+        """
 
         try:
             if request.is_json is False:
