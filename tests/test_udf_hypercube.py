dfe1014736cd808b8a61fd2a6ca4ae7946f2c225
huhabla <mo6i98ad>
2019-07-14 22:12:33 +0200

    Added message pack test example for hypercubes

--- tests/test_udf_hypercube.py
+++ tests/test_udf_hypercube.py
@@ -7,6 +7,8 @@ import pprint
 import unittest
 import xarray
 import numpy
+import msgpack
+import base64
 from openeo_udf.server.app import flask_api
 from openeo_udf.server.endpoints import create_endpoints
 from openeo_udf.server.definitions import UdfCode, UdfRequest
@@ -57,10 +59,34 @@ class AllTestCase(unittest.TestCase):
         pprint.pprint(udf_request)
 
         response = self.app.post('/udf', data=json.dumps(udf_request), content_type="application/json")
-        result = json.loads(response.data)
-        pprint.pprint(result)
+        dict_data = json.loads(response.data)
+        pprint.pprint(dict_data)
+        self.checkHyperCube(dict_data=dict_data)
 
-        udata = UdfData.from_dict(result)
+    def test_hypercube_ndvi_message_pack(self):
+        """Test the hypercube NDVI computation with the message pack protocol"""
+
+        dir = os.path.dirname(openeo_udf.functions.__file__)
+        file_name = os.path.join(dir, "hypercube_ndvi.py")
+        udf_code = UdfCode(language="python", source=open(file_name, "r").read())
+
+        hc_red = create_hypercube(name="red", value=1, shape=(3, 3, 3))
+        hc_nir = create_hypercube(name="nir", value=3, shape=(3, 3, 3))
+        udf_data = UdfData(proj={"EPSG":4326}, hypercube_list=[hc_red, hc_nir])
+
+        udf_request = UdfRequest(data=udf_data.to_dict(), code=udf_code)
+        # pprint.pprint(udf_request)
+
+        udf_request = base64.b64encode(msgpack.packb(udf_request, use_bin_type=True))
+        response = self.app.post('/udf_message_pack', data=udf_request, content_type="application/base64")
+
+        blob = base64.b64decode(response.data)
+        dict_data = msgpack.unpackb(blob, raw=False)
+        self.checkHyperCube(dict_data=dict_data)
+
+    def checkHyperCube(self, dict_data):
+        """Check the hyper cube data that was processed in the UDF server"""
+        udata = UdfData.from_dict(dict_data)
 
         hc_ndvi: HyperCube = udata.hypercube_list[0]
         self.assertEqual(hc_ndvi.id, "NDVI")
