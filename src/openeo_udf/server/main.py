91c7f25156af638b4be98dee90c9ccbaceda5db5
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-04 12:21:30 +0200

    Moved ml storage path to endpoint creation

--- src/openeo_udf/server/main.py
+++ src/openeo_udf/server/main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-import os
 from openeo_udf.server.app import flask_app
 from openeo_udf.server.endpoints import create_endpoints
-from openeo_udf.server.config import UdfConfiguration
 
 
 __license__ = "Apache License, Version 2.0"
@@ -14,8 +12,4 @@ __email__      = "soerengebbert@googlemail.com"
 create_endpoints()
 
 if __name__ == '__main__':
-    # Create the machine learn storage path
-    if not os.path.isdir(UdfConfiguration.machine_learn_storage_path):
-        os.mkdir(UdfConfiguration.machine_learn_storage_path)
-
     flask_app.run(host='0.0.0.0', port=8081, debug=True)
