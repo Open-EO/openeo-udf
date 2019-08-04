3aba2c815604d578a6bfd38f70ed9e2013ee7dd8
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-04 12:20:54 +0200

    Create the machine model storage path

--- src/openeo_udf/server/endpoints.py
+++ src/openeo_udf/server/endpoints.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+from openeo_udf.server.config import UdfConfiguration
 from openeo_udf.server.app import flask_api
 from openeo_udf.server.udf import Udf
+from openeo_udf.server.machine_learn_database import MachineLearnDatabase
 
 __license__ = "Apache License, Version 2.0"
 __author__     = "Soeren Gebbert"
@@ -18,7 +21,12 @@ def create_endpoints():
     """
     global created
 
+    # Create the machine learn storage path
+    if not os.path.isdir(UdfConfiguration.machine_learn_storage_path):
+        os.mkdir(UdfConfiguration.machine_learn_storage_path)
+
     if created is False:
         flask_api.add_resource(Udf, '/udf')
+        flask_api.add_resource(MachineLearnDatabase, '/ml_storage')
         created = True
 
