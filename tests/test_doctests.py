becedfa5f1321254a91cddafd184078f1a414dee
huhabla <mo6i98ad>
2019-08-11 12:17:10 +0200

    Added structured data and custom udf parameter doctests

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -5,7 +5,8 @@ Temporal framework doctests
 
 import doctest
 import unittest
-from openeo_udf.api import collection_tile, feature_collection_tile, hypercube, machine_learn_model, spatial_extent, udf_data
+from openeo_udf.api import collection_tile, feature_collection_tile, hypercube, \
+    machine_learn_model, spatial_extent, udf_data, structured_data, custom_udf_parameter
 
 
 def load_tests(loader, tests, ignore):
@@ -15,6 +16,8 @@ def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(hypercube))
     tests.addTests(doctest.DocTestSuite(machine_learn_model))
     tests.addTests(doctest.DocTestSuite(spatial_extent))
+    tests.addTests(doctest.DocTestSuite(structured_data))
+    tests.addTests(doctest.DocTestSuite(custom_udf_parameter))
     tests.addTests(doctest.DocTestSuite(udf_data))
     return tests
 
