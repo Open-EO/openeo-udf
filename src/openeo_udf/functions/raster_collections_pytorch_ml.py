0eae2f145fc6a1eee4eb151c516fa4095b2da748
huhabla <mo6i98ad>
2019-08-11 02:05:49 +0200

    Refacturing the base classes

--- src/openeo_udf/functions/raster_collections_pytorch_ml.py
+++ src/openeo_udf/functions/raster_collections_pytorch_ml.py
@@ -5,7 +5,9 @@ import torch
 #import torchvision
 #import tensorflow
 #import tensorboard
-from openeo_udf.api.base import SpatialExtent, RasterCollectionTile, FeatureCollectionTile, UdfData, MachineLearnModel
+
+from openeo_udf.api.raster_collection_tile import RasterCollectionTile
+from openeo_udf.api.udf_data import UdfData
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
