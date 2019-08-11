ac359da01d9261410af3786dcff3694217741b14
huhabla <mo6i98ad>
2019-08-12 01:08:30 +0200

    Use fastAPI instead of Flask

--- src/openeo_udf/server/machine_learn_database.py
+++ src/openeo_udf/server/machine_learn_database.py
@@ -6,14 +6,12 @@ from shutil import copyfile
 from hashlib import md5
 from os import listdir
 from os.path import isfile, join
-from typing import Optional
+from typing import Optional, List
 
 import requests
-from flask import make_response, jsonify, request
-from flask_restful import abort, Resource
-from flask_restful_swagger_2 import swagger
-
-from openeo_udf.server.udf_schemas import ErrorResponseSchema
+from fastapi import HTTPException, Body
+from openeo_udf.server.app import app
+from openeo_udf.server.udf_schemas import ErrorResponseModel
 from openeo_udf.server.config import UdfConfiguration
 
 
@@ -34,7 +32,7 @@ GET = {
         },
         "400": {
             "description": "The error message.",
-            "schema": ErrorResponseSchema
+            "schema": ErrorResponseModel
         }
     }
 }
@@ -53,7 +51,7 @@ POST = {
         },
         "400": {
             "description": "The error message.",
-            "schema": ErrorResponseSchema
+            "schema": ErrorResponseModel
         }
     }
 }
@@ -71,90 +69,88 @@ DELETE = {
         },
         "400": {
             "description": "The model was not found in the storage.",
-            "schema": ErrorResponseSchema
+            "schema": ErrorResponseModel
         }
     }
 }
 
 
-class MachineLearnDatabase(Resource):
-    """The machine learn model storage"""
+@app.get("/storage", response_model=List[str])
+def ml_get():
+    """Return all md5 hashes of the stored machine learn models as list
+    """
+    try:
+        path = UdfConfiguration.machine_learn_storage_path
+        if os.path.isdir(path):
+            hash_list = [f for f in listdir(path) if isfile(join(path, f))]
+            return hash_list
 
-    @swagger.doc(GET)
-    def get(self):
-        """Return all md5 hashes of the stored machine learn models as list
-        """
-        try:
-            path = UdfConfiguration.machine_learn_storage_path
-            if os.path.isdir(path):
-                hash_list = [f for f in listdir(path) if isfile(join(path, f))]
-                return make_response(jsonify(hash_list), 200)
+        response = ErrorResponseModel(message=f"The storage path of the machine learn models was not found on server.")
+        raise HTTPException(status_code=400, detail=response)
+    except Exception:
+        e_type, e_value, e_tb = sys.exc_info()
+        response = ErrorResponseModel(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
+        raise HTTPException(status_code=400, detail=response)
 
-            response = ErrorResponseSchema(message=f"The storage path of the machine learn models was not found on server.")
-            return make_response(jsonify(response), 400)
-        except Exception:
-            e_type, e_value, e_tb = sys.exc_info()
-            response = ErrorResponseSchema(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
-            return make_response(jsonify(response), 400)
 
-    @swagger.doc(DELETE)
-    def delete(self):
-        """Remove a single machine learn model from the server"""
-        md5_hash = str(request.data.decode('ascii'))
-        try:
-            path = os.path.join(UdfConfiguration.machine_learn_storage_path, md5_hash)
-            if os.path.exists(path):
-                os.remove(path)
-                return make_response(jsonify(md5_hash), 200)
+@app.delete("/storage/{md5_hash}", response_model=str)
+def ml_delete(md5_hash: str):
+    """Remove a single machine learn model from the server"""
 
-            response = ErrorResponseSchema(message=f"The machine learn model for hash {md5_hash} was not found")
-            return make_response(jsonify(response), 400)
-        except Exception:
-            e_type, e_value, e_tb = sys.exc_info()
-            response = ErrorResponseSchema(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
-            return make_response(jsonify(response), 400)
-
-    @swagger.doc(POST)
-    def post(self):
-        """Upload a new machine learn model to the UDF machine learn storage"""
-
-        try:
-            url = str(request.data.decode('ascii'))
-            if os.path.exists(url):
-                filepath = url
-            else:
-                # Check if thr URL exists by investigating the HTTP header
-                resp = requests.head(url, allow_redirects=True)
-
-                if resp.status_code != 200:
-                    raise Exception("The URL <%s> can not be accessed." % url)
-
-                filename = url.rsplit('/', 1)[1]
-                filepath = os.path.join(UdfConfiguration.temporary_storage_path, filename)
-                r = requests.get(url, allow_redirects=True)
-                open(filepath, 'wb').write(r.content)
-
-            md5_hash = self._compute_md5_hash(filepath)
-            if md5_hash:
-                return make_response(jsonify(md5_hash), 200)
-
-            response = ErrorResponseSchema(message=f"Unable to access machine learn model at {url}")
-            return make_response(jsonify(response), 400)
-        except Exception:
-            e_type, e_value, e_tb = sys.exc_info()
-            response = ErrorResponseSchema(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
-            return make_response(jsonify(response), 400)
-
-    @staticmethod
-    def _compute_md5_hash(filepath: str) -> Optional[str]:
-
-        if os.path.exists(filepath) and os.path.isfile(filepath):
-            md5_hash = md5(open(filepath, "rb").read()).hexdigest()
-            md5_hash_path = os.path.join(UdfConfiguration.machine_learn_storage_path, md5_hash)
-
-            if os.path.exists(md5_hash_path):
-                return md5_hash
-
-            copyfile(filepath, md5_hash_path)
+    try:
+        path = os.path.join(UdfConfiguration.machine_learn_storage_path, md5_hash)
+        if os.path.exists(path):
+            os.remove(path)
             return md5_hash
-        return None
+
+        response = ErrorResponseModel(message=f"The machine learn model for hash {md5_hash} was not found")
+        raise HTTPException(status_code=400, detail=response)
+    except Exception:
+        e_type, e_value, e_tb = sys.exc_info()
+        response = ErrorResponseModel(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
+        raise HTTPException(status_code=400, detail=response)
+
+
+@app.delete("/storage", response_model=str)
+def ml_post(url: str = Body(...)):
+    """Upload a new machine learn model to the UDF machine learn storage"""
+
+    try:
+        if os.path.exists(url):
+            filepath = url
+        else:
+            # Check if thr URL exists by investigating the HTTP header
+            resp = requests.head(url, allow_redirects=True)
+
+            if resp.status_code != 200:
+                raise Exception("The URL <%s> can not be accessed." % url)
+
+            filename = url.rsplit('/', 1)[1]
+            filepath = os.path.join(UdfConfiguration.temporary_storage_path, filename)
+            r = requests.get(url, allow_redirects=True)
+            open(filepath, 'wb').write(r.content)
+
+        md5_hash = compute_md5_hash(filepath)
+        if md5_hash:
+            return md5_hash
+
+        response = ErrorResponseModel(message=f"Unable to access machine learn model at {url}")
+        raise HTTPException(status_code=400, detail=response)
+    except Exception:
+        e_type, e_value, e_tb = sys.exc_info()
+        response = ErrorResponseModel(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
+        raise HTTPException(status_code=400, detail=response)
+
+
+def compute_md5_hash(filepath: str) -> Optional[str]:
+
+    if os.path.exists(filepath) and os.path.isfile(filepath):
+        md5_hash = md5(open(filepath, "rb").read()).hexdigest()
+        md5_hash_path = os.path.join(UdfConfiguration.machine_learn_storage_path, md5_hash)
+
+        if os.path.exists(md5_hash_path):
+            return md5_hash
+
+        copyfile(filepath, md5_hash_path)
+        return md5_hash
+    return None
