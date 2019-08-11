ab9b01f9ff2fddab6c996cf6854ddb56f31dc2f6
huhabla <mo6i98ad>
2019-08-11 02:05:36 +0200

    Refacturing the base classes

--- src/openeo_udf/functions/hypercube_ndvi.py
+++ src/openeo_udf/functions/hypercube_ndvi.py
@@ -6,8 +6,8 @@
 #import torchvision
 #import tensorflow
 #import tensorboard
-from openeo_udf.api.base import SpatialExtent, RasterCollectionTile, FeatureCollectionTile, UdfData
-from openeo_udf.api.base import UdfData, HyperCube
+from openeo_udf.api.hypercube import HyperCube
+from openeo_udf.api.udf_data import UdfData
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
