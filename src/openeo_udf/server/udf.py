b756b87953bc409890d7e244de8c1fd12e8615fe
huhabla <mo6i98ad>
2019-08-12 01:09:06 +0200

    Use fastAPI instead of Flask

--- src/openeo_udf/server/udf.py
+++ src/openeo_udf/server/udf.py
@@ -3,10 +3,9 @@ import traceback
 import sys
 import msgpack
 import base64
-from flask import make_response, jsonify, request
-from flask_restful import Resource
-from flask_restful_swagger_2 import swagger
-from openeo_udf.server.udf_schemas import UdfDataSchema, UdfRequestSchema, ErrorResponseSchema
+from fastapi import HTTPException
+from openeo_udf.server.app import app
+from openeo_udf.server.udf_schemas import UdfRequestModel, ErrorResponseModel
 from openeo_udf.api.run_code import run_json_user_code
 
 __license__ = "Apache License, Version 2.0"
@@ -56,99 +55,31 @@ Out[10]: {1: [1, 2, 3, 4, 5, 6], b'w': b'fffff', b'd': {b'd': b'd'}}
 """
 
 
-POST_JOBS_DOC_UDF = {
-    "description": "Run a Python user defined function (UDF) on the provided data",
-    "tags": ["UDF"],
-    "parameters": [
-        {
-            "name": "data",
-            "in": "body",
-            'required': True,
-            "description": "The UDF Python source code and data as JSON definition to process",
-            "schema": UdfRequestSchema
-        }
-    ],
-    'consumes':['application/json'],
-    'produces':["application/json"],
-    "responses": {
-        "200": {
-            "description": "The result of the UDF computation.",
-            "schema": UdfDataSchema
-        },
-        "400": {
-            "description": "The error message.",
-            "schema": ErrorResponseSchema
-        }
-    }
-}
+@app.post("/udf", response_model=UdfRequestModel)
+def udf_json(request: UdfRequestModel):
+    """Run a Python user defined function (UDF) on the provided data"""
+
+    try:
+        result = run_json_user_code(dict_data=request.dict())
+    except Exception:
+        e_type, e_value, e_tb = sys.exc_info()
+        response = ErrorResponseModel(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
+        raise HTTPException(status_code=400, detail=response)
+
+    return result
 
 
-class Udf(Resource):
-    @swagger.doc(POST_JOBS_DOC_UDF)
-    def post(self):
-        """Execute the UDF code
+@app.post("/udf_message_pack", response_model=UdfRequestModel)
+def udf_message_pack(request: str):
+    """Run a Python user defined function (UDF) on the provided data that are base64 encoded message pack"""
 
-        """
+    try:
+        blob = base64.b64decode(request)
+        dict_data = msgpack.unpackb(blob, raw=False)
+        result = run_json_user_code(dict_data=dict_data)
+    except Exception:
+        e_type, e_value, e_tb = sys.exc_info()
+        response = ErrorResponseModel(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
+        raise HTTPException(status_code=400, detail=response)
 
-        try:
-            if request.is_json is False:
-                raise Exception("Missing JSON in request")
-
-            json_data = request.get_json()
-            result = run_json_user_code(dict_data=json_data)
-        except Exception:
-            e_type, e_value, e_tb = sys.exc_info()
-            response = ErrorResponseSchema(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
-            return make_response(jsonify(response), 400)
-
-        return make_response(jsonify(result), 200)
-
-
-POST_JOBS_DOC_UDF_MESSAGE_PACK = {
-    "description": "Run a Python user defined function (UDF) on the provided data that are base64 encoded message pack"
-                   "binary data",
-    "tags": ["UDF"],
-    "parameters": [
-        {
-            "name": "data",
-            "in": "body",
-            'required': True,
-            "description": "The UDF Python source code and data as base64 encoded message pack",
-            "schema": UdfRequestSchema
-        }
-    ],
-    'consumes':['application/base64'],
-    'produces':["application/base64"],
-    "responses": {
-        "200": {
-            "description": "The result of the UDF computation as base64 encoded message pack.",
-            "schema": UdfDataSchema
-        },
-        "400": {
-            "description": "The error message.",
-            "schema": ErrorResponseSchema
-        }
-    }
-}
-
-
-class UdfMessagePack(Resource):
-    @swagger.doc(POST_JOBS_DOC_UDF_MESSAGE_PACK)
-    def post(self):
-        """Execute the UDF code that is encoded as base64 message pack binary format
-        """
-
-        try:
-            if request.is_json is True:
-                raise Exception("JSON is not supported in request. A base64 encoded message pack blob is required.")
-
-            blob = base64.b64decode(request.data)
-            dict_data = msgpack.unpackb(blob, raw=False)
-            result = run_json_user_code(dict_data=dict_data)
-        except Exception:
-            e_type, e_value, e_tb = sys.exc_info()
-            response = ErrorResponseSchema(message=str(e_value), traceback=str(traceback.format_tb(e_tb)))
-            return make_response(jsonify(response), 400)
-
-        result = base64.b64encode(msgpack.packb(result))
-        return make_response(result, 200)
+    return result
