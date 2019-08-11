052113090a8311cdf5a5a89e6b7432ea5242a28c
huhabla <mo6i98ad>
2019-08-12 01:24:11 +0200

    Import the important classes

--- src/openeo_udf/server/main.py
+++ src/openeo_udf/server/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from openeo_udf.server.app import app
 from openeo_udf.server.endpoints import create_storage_directory
-
+import openeo_udf.server.udf
+import openeo_udf.server.machine_learn_database
 
 __license__ = "Apache License, Version 2.0"
 __author__     = "Soeren Gebbert"
@@ -11,5 +12,3 @@ __email__      = "soerengebbert@googlemail.com"
 
 create_storage_directory()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
