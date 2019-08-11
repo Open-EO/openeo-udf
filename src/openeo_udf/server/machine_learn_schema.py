58bec3705bf5fde2acfca27fd5d2866134d1537e
huhabla <mo6i98ad>
2019-08-12 01:08:36 +0200

    Use fastAPI instead of Flask

--- src/openeo_udf/server/machine_learn_schema.py
+++ src/openeo_udf/server/machine_learn_schema.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask_restful_swagger_2 import Schema
+from pydantic import BaseModel, Schema as pydSchema
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -8,36 +8,26 @@ __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 
-class MachineLearnModelSchema(Schema):
-    description = "A machine learn model that should be applied to the UDF data."
-    type = "object"
-    required = ["framework", "path"]
-    properties = {
-        "framework": {
-            "type": "string",
-            "description": "The framework that was used to train the model",
-            "enum": ["sklearn", "pytorch", "tensorflow", "R"]
-        },
-        "name": {
-            "type": "string",
-            "description": "The name of the machine learn model."
-        },
-        "description": {
-            "type": "string",
-            "description": "The description of the machine learn model."
-        },
-        "path": {
-            "type": "string",
-            "description": "The path to the machine learn model file to which the UDF must have read access."
-        },
-        "md5_nash": {
-            "type": "string",
-            "description": "The md5 checksum of the model that should be used to identify "
-                           "the machine learn model in the UDF storage system. "
-                           "The machine learn model must be uploaded to the UDF server."
-        }
-    }
-    example = {"framework": "sklearn",
-               "name": "random_forest",
-               "description": "A random forest model",
-               "path": "/tmp/model.pkl.xz"}
+EXAMPLE = {"framework": "sklearn",
+           "name": "random_forest",
+           "description": "A random forest model",
+           "path": "/tmp/model.pkl.xz"}
+
+
+class MachineLearnModel(BaseModel):
+    """A machine learn model that should be applied to the UDF data."""
+
+    framework: str = pydSchema(..., description="The framework that was used to train the model",
+                               enum=["sklearn", "pytorch", "tensorflow", "R"],
+                               examples=[{"framework": "sklearn"}])
+
+    name: str = pydSchema(..., description="The name of the machine learn model.")
+
+    description: str = pydSchema(..., description="The description of the machine learn model.")
+
+    path: str = pydSchema(None, description="The path to the machine learn model file "
+                                            "to which the UDF must have read access.")
+
+    md5_hash: str = pydSchema(None, description="The md5 checksum of the model that should be used to identify "
+                                                "the machine learn model in the UDF storage system. "
+                                                "The machine learn model must be uploaded to the UDF server.")
