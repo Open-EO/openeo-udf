a864ff07d8b11c5784ea9993542da32f8440ab2b
huhabla <mo6i98ad>
2019-08-12 01:08:47 +0200

    Use fastAPI instead of Flask

--- src/openeo_udf/server/raster_collection_tile_schema.py
+++ src/openeo_udf/server/raster_collection_tile_schema.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask_restful_swagger_2 import Schema
+from typing import List
 
-from openeo_udf.server.spatial_extent_schema import SpatialExtentSchema
+from pydantic import BaseModel, Schema as pydSchema
+
+from openeo_udf.server.spatial_extent_schema import SpatialExtentModel
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -10,77 +12,74 @@ __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 
-class RasterCollectionTileSchema(Schema):
-    description = "A three dimensional tile of data that represents a spatio-temporal " \
-                  "subset of a spatio-temporal raster collection. "
-    type = "object"
-    required = ["data", "id", "extent"]
-    properties = {
-        "id": {
-            "description": "The identifier of this raster collection tile.",
-            "type": "string"
-        },
-        "wavelength": {
-            "description": "The wavelength of the raster collection tile.",
-            "type": "float"
-        },
-        "data": {
-
-            "description": "A three dimensional array of integer (8,16,32,64 bit) or float (16, 32, 64 bit) values." \
-                           "The index dimension is as follows: [time][y][x]. Hence, the index data[0] returns " \
-                           "the 2D slice for the first time-stamp. The y-indexing if counted from top to bottom " \
-                           "and represents the rows of the 2D array. The x-indexing is counted from left to right " \
-                           "and represents the columns of the 2D array.",
-            "type": "array",
-            "items": {
-                "type": "array",
-                "items": {
-                    "type": "array",
-                    "items": {"type": "float"}
-                }
-            }
-        },
-        "start_times": {
-            "description": "The array that contains that start time values for each x,y slice."
-                           "As date-time string format ISO 8601 must be supported.",
-            "type": "array",
-            "items": {"type": "string"}
-        },
-        "end_times": {
-            "description": "The vector that contains that end time values for each x,y slice, in case the "
-                           "the time stamps for all or a subset of slices are intervals. For time instances "
-                           "the from and to time stamps must be equal or empty. "
-                           "As date-time string format ISO 8601 must "
-                           "be supported",
-            "type": "array",
-            "items": {"type": "string"}
-        },
-        "extent": SpatialExtentSchema
-    }
-    example = {
-        "id": "test_data",
-        "wavelength": 420,
-        "start_times": ["2001-01-01T00:00:00",
-                        "2001-01-02T00:00:00"],
-        "end_times": ["2001-01-02T00:00:00",
-                      "2001-01-03T00:00:00"],
-        "data": [
-            [
-                [0, 1],
-                [2, 3]
-            ],
-            [
-                [0, 1],
-                [2, 3]
-            ]
+EXAMPLE = {
+    "id": "test_data",
+    "wavelength": 420,
+    "start_times": ["2001-01-01T00:00:00",
+                    "2001-01-02T00:00:00"],
+    "end_times": ["2001-01-02T00:00:00",
+                  "2001-01-03T00:00:00"],
+    "data": [
+        [
+            [0, 1],
+            [2, 3]
         ],
-        "extent": {
-            "top": 53,
-            "bottom": 51,
-            "right": 30,
-            "left": 28,
-            "height": 1,
-            "width": 1
-        }
+        [
+            [0, 1],
+            [2, 3]
+        ]
+    ],
+    "extent": {
+        "top": 53,
+        "bottom": 51,
+        "right": 30,
+        "left": 28,
+        "height": 1,
+        "width": 1
     }
+}
 
+
+class RasterCollectionTileModel_(BaseModel):
+    """A three dimensional tile of data that represents a spatio-temporal
+    subset of a spatio-temporal raster collection."""
+
+    id: str = pydSchema(None, description="The identifier of this raster collection tile.")
+
+    wavelength: float = pydSchema(None, description="The wavelength of the raster collection tile.")
+
+    data: List[List[List[float]]] = pydSchema(None,
+                                              description="A three dimensional array of integer "
+                                                          "(8,16,32,64 bit) or float (16, 32, 64 bit) values."
+                                                          "The index dimension is as follows: [time][y][x]. "
+                                                          "Hence, the index data[0] returns "
+                                                          "the 2D slice for the first time-stamp. "
+                                                          "The y-indexing if counted from top to bottom "
+                                                          "and represents the rows of the 2D array. "
+                                                          "The x-indexing is counted from left to right "
+                                                          "and represents the columns of the 2D array.",
+                                              examples=[{"data": [
+                                                  [
+                                                      [0.0, 0.1],
+                                                      [0.2, 0.3]
+                                                  ],
+                                                  [
+                                                      [0.0, 0.1],
+                                                      [0.2, 0.3]
+                                                  ]
+                                              ]}])
+
+    start_time: List[str] = pydSchema(None,
+                                      description="The array that contains that start time values for "
+                                                  "each raster slice feature. As date-time string format "
+                                                  "ISO 8601 must be supported.",
+                                      examples=[{"start_times": ["2001-01-01T00:00:00", "2001-01-02T00:00:00"]}])
+
+    end_time: List[str] = pydSchema(None,
+                                    description="The vector that contains that end time values for each "
+                                                "raster slice, in case the "
+                                                "the time stamps for all or a subset of slices are intervals. "
+                                                "For time instances the from and to time stamps must be equal or empty."
+                                                "As date-time string format ISO 8601 must be supported.",
+                                    examples=[{"end_times": ["2001-01-02T00:00:00", "2001-01-03T00:00:00"]}])
+    extent: SpatialExtentModel
