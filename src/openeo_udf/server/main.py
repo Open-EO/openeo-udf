d8567b847ce479a53ae4cf20b231a90e6915181e
huhabla <mo6i98ad>
2019-08-12 01:08:41 +0200

    Use fastAPI instead of Flask

--- src/openeo_udf/server/main.py
+++ src/openeo_udf/server/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from openeo_udf.server.app import flask_app
-from openeo_udf.server.endpoints import create_endpoints
+from openeo_udf.server.app import app
+from openeo_udf.server.endpoints import create_storage_directory
 
 
 __license__ = "Apache License, Version 2.0"
@@ -9,7 +9,7 @@ __copyright__  = "Copyright 2018, Soeren Gebbert"
 __maintainer__ = "Soeren Gebbert"
 __email__      = "soerengebbert@googlemail.com"
 
-create_endpoints()
+create_storage_directory()
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
