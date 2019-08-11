c8b71643619b2cc07c1a46deb0cbf88cf878ccd2
huhabla <mo6i98ad>
2019-08-11 02:06:50 +0200

    Refacturing the base classes

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -5,12 +5,17 @@ Temporal framework doctests
 
 import doctest
 import unittest
-from openeo_udf.api import base
+from openeo_udf.api import collection_tile, feature_collection_tile, hypercube, machine_learn_model, spatial_extent, udf_data
 
 
 def load_tests(loader, tests, ignore):
     """Load all doctests from the base implementation as unittests"""
-    tests.addTests(doctest.DocTestSuite(base))
+    tests.addTests(doctest.DocTestSuite(collection_tile))
+    tests.addTests(doctest.DocTestSuite(feature_collection_tile))
+    tests.addTests(doctest.DocTestSuite(hypercube))
+    tests.addTests(doctest.DocTestSuite(machine_learn_model))
+    tests.addTests(doctest.DocTestSuite(spatial_extent))
+    tests.addTests(doctest.DocTestSuite(udf_data))
     return tests
 
 
