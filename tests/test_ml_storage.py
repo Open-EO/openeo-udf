74a59e638f9febecb1585950d989c2748582c1c3
huhabla <mo6i98ad>
2019-08-12 11:49:52 +0200

    Ported tests to fastapi

--- tests/test_ml_storage.py
+++ tests/test_ml_storage.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
+
 from flask import json
 import unittest
 from hashlib import md5
@@ -32,16 +34,16 @@ class MachineLearningModelStorageTestCase(unittest.TestCase):
         file.write(content)
         file.close()
 
-        response = self.app.post('/storage', data=path, content_type="text/plain")
-        print(response.data)
+        response = self.app.post('/storage', data=path, headers={"Content-Type": "text/plain"})
+        #print(response.content)
         self.assertEqual(response.status_code, 200)
 
         response = self.app.get('/storage')
-        print(response.data)
+        #print(response.content)
         self.assertEqual(response.status_code, 200)
 
         md5_hash = md5(content).hexdigest()
-        print(response.data)
+        #print(response.content)
         response = self.app.delete(f'/storage/{md5_hash}')
         self.assertEqual(response.status_code, 200)
 
@@ -49,19 +51,27 @@ class MachineLearningModelStorageTestCase(unittest.TestCase):
 
         url = "https://storage.googleapis.com/datentransfer/europe_countries.geojson"
 
-        response = self.app.post('/storage', data=url, content_type="text/plain")
-        print(response.data)
+        response = self.app.post('/storage', data=url, headers={"Content-Type": "text/plain"})
+        #print(response.content)
         self.assertEqual(response.status_code, 200)
 
         response = self.app.get('/storage')
-        print(response.data)
+        #print(response.content)
         self.assertEqual(response.status_code, 200)
 
-        md5_hash = json.loads(response.data)[0]
+        md5_hash = response.json()[0]
 
         response = self.app.delete(f'/storage/{md5_hash}')
-        print(response.data)
+        #print(response.content)
         self.assertEqual(response.status_code, 200)
+
+    def test_ml_storage_post_url_error(self):
+
+        url = "https://nopopopop.de/file.txt"
+
+        response = self.app.post('/storage', data=url, headers={"Content-Type": "text/plain"})
+        # pprint(response.json())
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == "__main__":
