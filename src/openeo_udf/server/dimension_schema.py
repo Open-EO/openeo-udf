008b1d7fc4ac4e196dc2bda1812047c0b1ee777e
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-13 18:43:10 +0200

    Fixed Schema name

--- src/openeo_udf/server/dimension_schema.py
+++ src/openeo_udf/server/dimension_schema.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import List, Union
 
-from pydantic import BaseModel, Schema as pydSchema
+from pydantic import BaseModel, Schema
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -13,14 +13,14 @@ __email__ = "soerengebbert@googlemail.com"
 class DimensionModel(BaseModel):
     """Description of a single dimension"""
 
-    name: str = pydSchema(...,
-                          description="The name of the dimension, like *time*, *X*, *Y*, *Z* and so on.",
-                          examples=[{"name": "time"}])
+    name: str = Schema(...,
+                       description="The name of the dimension, like *time*, *X*, *Y*, *Z* and so on.",
+                       examples=[{"name": "time"}])
 
-    description: str = pydSchema(None, description="Description of the dimension.")
+    description: str = Schema(None, description="Description of the dimension.")
 
-    unit: str = pydSchema(None,
-                          description="The unit of the dimension. The unit can be *ISO:8601* for time; "
+    unit: str = Schema(None,
+                       description="The unit of the dimension. The unit can be *ISO:8601* for time; "
                                       "metric length units based on meter: *nm* (nanometer), "
                                       "*mm* (millimeter), *cm* (centimeter), "
                                       "*m* (meter), *dm* (decimeter), *km* (kilometer); "
@@ -29,10 +29,10 @@ class DimensionModel(BaseModel):
                                       "earth observation units: NDVI, DVI, ... ; "
                                       "sensor units: *int8*, *int16*, *int32*; "
                                       "user defined units: *user_lala* ",
-                          examples=[{"unit": "ISO:8601"}])
+                       examples=[{"unit": "ISO:8601"}])
 
-    coordinates: List[Union[int, float, str]] = pydSchema(None,
-                                                          description="The array that contains the coordinates "
+    coordinates: List[Union[int, float, str]] = Schema(None,
+                                                       description="The array that contains the coordinates "
                                                                       "of the specific dimension. "
                                                                       "This parameter is optional.",
-                                                          examples=[{"coordinates": ["2001-01-01", "2001-01-02"]}])
+                                                       examples=[{"coordinates": ["2001-01-01", "2001-01-02"]}])
