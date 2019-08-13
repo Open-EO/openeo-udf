c9704cf7ea5768184b767a6d0935e86615a6a54c
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-13 18:43:41 +0200

    Fixed Schema name, added Example

--- src/openeo_udf/server/feature_collection_tile_schema.py
+++ src/openeo_udf/server/feature_collection_tile_schema.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, List
 
-from pydantic import BaseModel, Schema as pydSchema
+from pydantic import BaseModel, Schema
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -28,25 +28,30 @@ class FeatureCollectionTileModel(BaseModel):
     """A tile of vector data that represents a spatio-temporal
     subset of a spatio-temporal vector dataset."""
 
-    id: str = pydSchema(..., description="The identifier of this vector tile.", examples=[{"id": "test_data"}])
+    id: str = Schema(..., description="The identifier of this vector tile.", examples=[{"id": "test_data"}])
 
-    data: Dict = pydSchema(..., description="A GeoJSON FeatureCollection.",
-                           examples=[
+    data: Dict = Schema(..., description="A GeoJSON FeatureCollection.",
+                        examples=[
                                {"data": {"features": [{"id": "0", "type": "Feature", "properties": {"a": 1, "b": "a"},
                                                        "geometry": {"coordinates": [24.0, 50.0], "type": "Point"}},
                                                       {"id": "1", "type": "Feature", "properties": {"a": 2, "b": "b"},
                                                        "geometry": {"coordinates": [30.0, 53.0], "type": "Point"}}],
                                          "type": "FeatureCollection"}}])
 
-    start_time: List[str] = pydSchema(None, description="The array that contains that start time values for "
+    start_time: List[str] = Schema(None, description="The array that contains that start time values for "
                                                         "each vector feature. As date-time string format "
                                                         "ISO 8601 must be supported.",
-                                      examples=[{"start_times": ["2001-01-01T00:00:00", "2001-01-02T00:00:00"]}])
+                                   examples=[{"start_times": ["2001-01-01T00:00:00", "2001-01-02T00:00:00"]}])
 
-    end_time: List[str] = pydSchema(None,
-                                    description="The vector that contains that end time values for each "
+    end_time: List[str] = Schema(None,
+                                 description="The vector that contains that end time values for each "
                                                 "vector feature, in case the "
                                                 "the time stamps for all or a subset of slices are intervals. "
                                                 "For time instances the from and to time stamps must be equal or empty."
                                                 "As date-time string format ISO 8601 must be supported.",
-                                    examples=[{"end_times": ["2001-01-02T00:00:00", "2001-01-03T00:00:00"]}])
+                                 examples=[{"end_times": ["2001-01-02T00:00:00", "2001-01-03T00:00:00"]}])
+
+    class Config:
+        schema_extra = {
+            'examples': [EXAMPLE]
+        }
