c7af948059a02ca1aa47431a803b8ad9ea118f33
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-13 18:44:17 +0200

    Fixed Schema name, added Example

--- src/openeo_udf/server/raster_collection_tile_schema.py
+++ src/openeo_udf/server/raster_collection_tile_schema.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import List
 
-from pydantic import BaseModel, Schema as pydSchema
+from pydantic import BaseModel, Schema
 
 from openeo_udf.server.spatial_extent_schema import SpatialExtentModel
 
@@ -44,12 +44,12 @@ class RasterCollectionTileModel(BaseModel):
     """A three dimensional tile of data that represents a spatio-temporal
     subset of a spatio-temporal raster collection."""
 
-    id: str = pydSchema(..., description="The identifier of this raster collection tile.")
+    id: str = Schema(..., description="The identifier of this raster collection tile.")
 
-    wavelength: float = pydSchema(None, description="The wavelength of the raster collection tile.")
+    wavelength: float = Schema(None, description="The wavelength of the raster collection tile.")
 
-    data: List[List[List[float]]] = pydSchema(...,
-                                              description="A three dimensional array of integer "
+    data: List[List[List[float]]] = Schema(...,
+                                           description="A three dimensional array of integer "
                                                           "(8,16,32,64 bit) or float (16, 32, 64 bit) values."
                                                           "The index dimension is as follows: [time][y][x]. "
                                                           "Hence, the index data[0] returns "
@@ -58,7 +58,7 @@ class RasterCollectionTileModel(BaseModel):
                                                           "and represents the rows of the 2D array. "
                                                           "The x-indexing is counted from left to right "
                                                           "and represents the columns of the 2D array.",
-                                              examples=[{"data": [
+                                           examples=[{"data": [
                                                   [
                                                       [0.0, 0.1],
                                                       [0.2, 0.3]
@@ -69,17 +69,22 @@ class RasterCollectionTileModel(BaseModel):
                                                   ]
                                               ]}])
 
-    start_time: List[str] = pydSchema(None,
-                                      description="The array that contains that start time values for "
+    start_time: List[str] = Schema(None,
+                                   description="The array that contains that start time values for "
                                                   "each raster slice feature. As date-time string format "
                                                   "ISO 8601 must be supported.",
-                                      examples=[{"start_times": ["2001-01-01T00:00:00", "2001-01-02T00:00:00"]}])
+                                   examples=[{"start_times": ["2001-01-01T00:00:00", "2001-01-02T00:00:00"]}])
 
-    end_time: List[str] = pydSchema(None,
-                                    description="The vector that contains that end time values for each "
+    end_time: List[str] = Schema(None,
+                                 description="The vector that contains that end time values for each "
                                                 "raster slice, in case the "
                                                 "the time stamps for all or a subset of slices are intervals. "
                                                 "For time instances the from and to time stamps must be equal or empty."
                                                 "As date-time string format ISO 8601 must be supported.",
-                                    examples=[{"end_times": ["2001-01-02T00:00:00", "2001-01-03T00:00:00"]}])
+                                 examples=[{"end_times": ["2001-01-02T00:00:00", "2001-01-03T00:00:00"]}])
     extent: SpatialExtentModel
+
+    class Config:
+        schema_extra = {
+            'examples': [EXAMPLE]
+        }
