b7d67b71f0ca40d7da7ea3ad434eb6cf4ff658fc
Soeren Gebbert <soerengebbert@gmail.com>
2019-08-13 18:43:50 +0200

    Fixed Schema name, added Example

--- src/openeo_udf/server/hypercube_schema.py
+++ src/openeo_udf/server/hypercube_schema.py
@@ -2,7 +2,7 @@
 from typing import List, Union, Any
 
 from openeo_udf.server.dimension_schema import DimensionModel
-from pydantic import BaseModel, Schema as pydSchema
+from pydantic import BaseModel, Schema
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -33,9 +33,9 @@ EXAMPLE = {
 class HyperCubeModel(BaseModel):
     """A multi dimensional hypercube with configurable dimensions."""
 
-    id: str = pydSchema(..., description="The identifier of this hyper cube.", examples=[{"id": "test_data"}])
+    id: str = Schema(..., description="The identifier of this hyper cube.", examples=[{"id": "test_data"}])
 
-    data: Any = pydSchema(..., description="A multi-dimensional array of integer (8,16,32,64 bit) "
+    data: Any = Schema(..., description="A multi-dimensional array of integer (8,16,32,64 bit) "
                                            "or float (16, 32, 64 bit) values."
                                            "By default index dimension is as follows: [time][y][x]. "
                                            "Hence, the index data[0] returns "
@@ -45,7 +45,7 @@ class HyperCubeModel(BaseModel):
                                            "The x-indexing is counted from left to right "
                                            "and represents the columns of the 2D array. "
                                            "The dimension options must be used ot describe ",
-                          examples=[{"data": [
+                       examples=[{"data": [
                               [
                                   [0.0, 0.1],
                                   [0.2, 0.3]
@@ -56,17 +56,22 @@ class HyperCubeModel(BaseModel):
                               ]
                           ]}])
 
-    dimensions: List[DimensionModel] = pydSchema(...,
-                                                  description="The description of each dimension "
+    dimensions: List[DimensionModel] = Schema(...,
+                                              description="The description of each dimension "
                                                               "and the value as ordered list. "
                                                               "The order of the dimension in this array "
                                                               "is the order of the dimension in the hypercube. "
                                                               "The dimension with the name "
                                                               "*value* described the cell value.",
-                                                  examples=[{"dimension": [{"name": "time", "unit": "ISO:8601",
+                                              examples=[{"dimension": [{"name": "time", "unit": "ISO:8601",
                                                                            "coordinates": ["2001-01-01", "2001-01-02"]},
                                                                           {"name": "X", "unit": "degree",
                                                                            "coordinates": [50.0, 60.0]},
                                                                           {"name": "Y", "unit": "degree",
                                                                            "coordinates": [40.0, 50.0]},
                                                                           ]}])
+
+    class Config:
+        schema_extra = {
+            'examples': [EXAMPLE]
+        }
