fef6aaa444b0d3ef8f51e0d5e861e1b3183d3dc8
Soeren Gebbert <soerengebbert@gmail.com>
2019-11-22 13:37:47 +0100

    More description

--- src/openeo_udf/server/data_exchange_model/data_cube.py
+++ src/openeo_udf/server/data_exchange_model/data_cube.py
@@ -14,20 +14,32 @@ class Dimension(BaseModel):
     description: str = Field(..., description="The description of the dimension.")
     type: str = Field(..., description="The type of the dimension (spatial, temporal, bands, other)")
     unit: str = Field(...,
-                      description="The unit of the dimension in SI units or UDUNITS.",
+                      description="The unit of the dimension in SI units or UDUNITS. "
+                                  "Time instances or intervals are defined as ISO8601 strings",
                       examples=[{"unit": "seconds"}, {"unit": "m"}, {"unit": "hours"},
-                                {"unit": "days"}, {"unit": "mm"}, {"unit": "km"}])
+                                {"unit": "days"}, {"unit": "mm"}, {"unit": "km"}, {"unit": "ISO8601"}])
     extent: List[Union[int, float, str]] = Field(..., description="The spatial or temporal extent of the dimension. "
                                                                   "It must be a tuple of values.")
-    values: List[Union[int, float, str]] = Field(None, description="A list of coordinates for this dimension")
-    step: Union[int, float, str] = Field(None, description="The step size of the dimension.")
+    values: List[Union[int, float, str]] = Field(None, description="A list of coordinates for this dimension. Use "
+                                                                   "ISO8601 to specify time instances and intervals."
+                                                                   "If spatial axis are irregular, then the values "
+                                                                   "parameter should be used to specify this axis. "
+                                                                   "Otherwise the extent and the number of cells "
+                                                                   "is sufficient.",
+                                                 examples=[{"values": [50, 51, 52]},
+                                                           {"values": ["2001-01-01T10:00:00 / 2001-01-01T12:00:00",
+                                                                       "2001-01-01T12:00:00 / 2001-01-01T14:00:00"]}])
+    number_of_cells: int = Field(None, description="The number of cells in the spatial dimension or intervals / "
+                                                   "time instances in case of temporal dimension.")
     axis: str = Field(None, description="If the dimension is spatial, then the axis x, y or z can be "
                                         "specified with this parameter.")
     reference_system: Union[str, int, Dict] = Field(None, description="The definition of the coordinate system. If an "
                                                                       "integer was provided, it will be interpreted "
                                                                       "as EPSG code. If a string was provided it will "
-                                                                      "be interpreted as WKT2 definition. In case of a "
-                                                                      "dictionary object PROJSON is expected.")
+                                                                      "be interpreted as WKT2 definition or in case "
+                                                                      "of a temporal dimension as the calendar. "
+                                                                      "In case of a dictionary object PROJSON "
+                                                                      "is expected.")
 
 
 class DataCube(BaseModel):
@@ -45,7 +57,10 @@ class DataCube(BaseModel):
                             examples=[[3, 3, 3]])
 
     dimensions: Dict[str, Dimension] = Field(..., description="A dictionary of dimension descriptions. Dimensions are "
-                                                              "references by their name that is the key of the dict.")
+                                                              "references by their name that is the key of the dict. "
+                                                              "The id of the dimension is a string, that should "
+                                                              "follow the convention: t -> time, "
+                                                              "x, y, z -> spatial dimensions.")
     field_collection: int = Field(None, description="The integer index of the field collection. All fields and their "
                                                     "values of this indexed collection are assigned to the "
                                                     "data cube and must have the same size")
