4c0962bb43e021fdb79c643fd2694fa4294b3cca
Soeren Gebbert <soerengebbert@gmail.com>
2019-11-22 14:35:32 +0100

    Updated to new format

--- tests/test_data_collection_data_cube.py
+++ tests/test_data_collection_data_cube.py
@@ -2,10 +2,10 @@
 
 import unittest
 
-from openeo_udf.server.data_exchange_model.data_cube import DataCube, Dimension
-from openeo_udf.server.data_exchange_model.field_collection import Field, FieldCollection
-from openeo_udf.server.data_exchange_model.metadata import Metadata
-from openeo_udf.server.data_exchange_model.data_collection import DataCollection, ObjectCollection, TimeStamps
+from openeo_udf.server.data_model.data_cube import DataCube, Dimension
+from openeo_udf.server.data_model.variables_collection import Variable, VariablesCollection
+from openeo_udf.server.data_model.metadata import Metadata
+from openeo_udf.server.data_model.data_collection import DataCollection, ObjectCollection, TimeStamps
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Soeren Gebbert"
@@ -33,13 +33,16 @@ class DataCollectionTest(unittest.TestCase):
         dim_t = Dimension(description="Temporal dimension", type="temporal", reference_system="gregorian",
                           unit="ISO:8601",
                           values=["2001-01-01T00:00:00", "2001-01-01T00:01:00", "2001-01-01T00:02:00"],
-                          extent=["2001-01-01T00:00:00", "2001-01-01T00:02:00"])
+                          extent=["2001-01-01T00:00:00", "2001-01-01T00:02:00"],
+                          number_of_cells=3)
         dim_dict["time"] = dim_t
         dim_x = Dimension(description="Spatial dimension", type="spatial", reference_system=4326, axis="x",
-                          unit="degree", values=[0, 1, 2], extent=[0, 2])
+                          unit="degree", values=[0, 1, 2], extent=[0, 2],
+                          number_of_cells=3)
         dim_dict["x"] = dim_x
         dim_y = Dimension(description="Spatial dimension", type="spatial", reference_system=4326, axis="y",
-                          unit="degree", step=1, extent=[0, 2])
+                          unit="degree", step=1, extent=[0, 2],
+                          number_of_cells=3)
         dim_dict["y"] = dim_y
 
         dc = DataCube(name="Data Cube", description="This is a data cube", dim=["time", "y", "x"], size=[3, 3, 3],
@@ -47,17 +50,17 @@ class DataCollectionTest(unittest.TestCase):
 
         # FIELD COLLECTION
 
-        t = Field(name="Temperature", description="Temperature", unit="degree celsius",
-                  values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+        t = Variable(name="Temperature", description="Temperature", unit="degree celsius",
+                     values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
                           27],
-                  labels=[])
+                     labels=[])
 
-        p1 = Field(name="Precipitation", description="Precipitation", unit="mm",
-                   values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        p1 = Variable(name="Precipitation", description="Precipitation", unit="mm",
+                      values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
                            26, 27],
-                   labels=[])
+                      labels=[])
 
-        f1 = FieldCollection(name="Climate data", size=[3, 3, 3], number_of_fields=2, fields=[t, p1])
+        f1 = VariablesCollection(name="Climate data", size=[3, 3, 3], number_of_variables=2, variables=[t, p1])
 
         # GEOMETRY COLLECTION
 
@@ -70,7 +73,7 @@ class DataCollectionTest(unittest.TestCase):
         ts = TimeStamps(calendar="gregorian", intervals=[("2001-01-01T10:00:00", "2001-01-01T00:02:00")])
 
         t = DataCollection(metadata=m, object_collections=oc, geometry_collection=g,
-                           field_collections=[f1], timestamps=ts)
+                           variables_collections=[f1], timestamps=ts)
 
         self.assertIsNotNone(t.json())
         print(t.json())
