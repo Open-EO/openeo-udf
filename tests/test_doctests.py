5c844a6e825a8e2140c41555509da1dc0ce2515f
Soeren Gebbert <soerengebbert@gmail.com>
2019-11-23 15:20:15 +0100

    Class renaming

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -5,14 +5,14 @@ Temporal framework doctests
 
 import doctest
 import unittest
-from openeo_udf.api import collection_tile, feature_collection_tile, hypercube, \
+from openeo_udf.api import collection_tile, feature_collection, hypercube, \
     machine_learn_model, spatial_extent, udf_data, structured_data
 
 
 def load_tests(loader, tests, ignore):
     """Load all doctests from the base implementation as unittests"""
     tests.addTests(doctest.DocTestSuite(collection_tile))
-    tests.addTests(doctest.DocTestSuite(feature_collection_tile))
+    tests.addTests(doctest.DocTestSuite(feature_collection))
     tests.addTests(doctest.DocTestSuite(hypercube))
     tests.addTests(doctest.DocTestSuite(machine_learn_model))
     tests.addTests(doctest.DocTestSuite(spatial_extent))
