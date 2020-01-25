8ec1666829138f19209b4b13422d3a55f7c92137
huhabla <mo6i98ad>
2020-01-25 22:49:52 +0100

    Use new function call approach

--- src/openeo_udf/functions/datacube_ndvi.py
+++ src/openeo_udf/functions/datacube_ndvi.py
@@ -44,8 +44,3 @@ def hyper_ndvi(udf_data: UdfData):
 
     hc = DataCube(array=ndvi)
     udf_data.set_datacube_list([hc, ])
-
-
-# This function call is the entry point for the UDF.
-# The caller will provide all required data in the **data** object.
-hyper_ndvi(data)
