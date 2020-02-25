988f70dbf29cc0de0f8785d68f42a50558d1ea16
huhabla <mo6i98ad>
2020-02-25 21:06:20 +0100

    Check data array conversion

--- tests/test_udf_datacube_api.py
+++ tests/test_udf_datacube_api.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
+import xarray
+import numpy
 
 from openeo_udf.server.app import app
 from starlette.testclient import TestClient
@@ -24,10 +26,30 @@ class DataCubeApiTestCase(unittest.TestCase):
     def test_hypercube_api(self):
         """Test the hypercube mean reduction"""
 
-        dc = create_data_collection_model_example()
-        dc = DataCube.from_data_collection(data_collection=dc, model_index=0)
+        dcm = create_data_collection_model_example()
+        dc = DataCube.from_data_collection(data_collection=dcm, model_index=0)
         print(dc[0].get_array())
         print(dc[1].get_array())
+
+        dc1: DataCube = dc[0]
+        dc2: DataCube = dc[1]
+
+        self.assertEqual(dc1.id, dcm.variables_collections[0].variables[0].name)
+        self.assertEqual(dc2.id, dcm.variables_collections[0].variables[1].name)
+
+        a1: xarray.DataArray = dc1.get_array()
+        a1 = numpy.asarray(a1).reshape([27])
+        v1 = dcm.variables_collections[0].variables[0].values
+        v1 = numpy.asarray(v1)
+        self.assertTrue(a1.all() == v1.all())
+
+        a2: xarray.DataArray = dc2.get_array()
+        a2 = numpy.asarray(a2).reshape([27])
+        v2 = dcm.variables_collections[0].variables[1].values
+        v2 = numpy.asarray(v2)
+        self.assertTrue(a2.all() == v2.all())
+
+
 
 
 if __name__ == "__main__":
