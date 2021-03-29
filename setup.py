8a43c7fbc556cb23da52935c8a94e60515a9cd47
Jeroen Dries <jeroen.dries@vito.be>
2021-03-29 13:36:37 +0200

    removed empty entrypoints, may cause issue with importlib-metadata==3.9.0

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-          entry_points=entry_points,
           use_pyscaffold=True,
           scripts=['scripts/run_udf_server'])
 
